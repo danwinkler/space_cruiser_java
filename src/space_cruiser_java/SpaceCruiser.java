52d750e51b1c3e974e15c09d4993715620ae7222
Daniel Winkler <danthemanwinkler@gmail.com>
2014-11-04 01:49:20 -0600

    testing default screens

--- src/space_cruiser_java/SpaceCruiser.java
+++ src/space_cruiser_java/SpaceCruiser.java
@@ -1,17 +1,25 @@
 package space_cruiser_java;
 
 import game_framework.DGame;
+import game_framework.ScreenManager;
 
 import com.danwink.game_framework.screens.TitleScreen;
 
 public class SpaceCruiser 
 {
+	NetworkServer
+	
 	public static void main( String[] args )
 	{
 		DGame g = new DGame( "Space Cruiser", 800, 600 );
 		
 		g.begin( () -> {
-			g.add( "title", new TitleScreen( "Space Cruiser" ) );
+			g.add( "title", new TitleScreen( "Space Cruiser" ) {{
+				setTitleScale( 2 );
+				addButton( "Start Local", () -> {
+					ScreenManager.getScreenManager( this ).activate( "play" );
+				});
+			}});
 			
 			g.add( "play", new PlayScreen() );
 			
